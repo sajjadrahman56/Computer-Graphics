ab346ccee59b8e2709ac87848e55c9f252e3c700
sajjad-njr <sajjadrahman1020@gmail.com>
2023-05-17 23:24:52 +0600

    Circle of the breshenham circle

---
+++ Circle/breshenham_circle.cpp
@@ -0,0 +1,64 @@
+//bresenham's circle algorithm
+/**
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+#include<graphics.h>
+
+void circlebres(int x1,int y1,int x, int y);
+
+int main(){
+
+    int x1,y1,r, x, y,d;
+
+    int gd, gm;
+    gd = DETECT;
+
+    cout<<"Enter the center of a circle: ";
+	cin>>x1>>y1;
+	cout<<"Enter the value of radius: ";
+	cin>>r;
+
+    initgraph(&gd, &gm, "");
+
+    x = 0;
+    y = r;
+    d = 3 - 2*r;
+
+    circlebres(x1,y1,x,y);
+
+    while(x<y)
+	{
+
+		if(d<0)
+		{
+		    x++;
+			d=d+4*x+6;
+		}
+		else
+		{
+		    x++;
+		    y--;
+			d=d+4*(x-y)+10;
+		}
+		circlebres(x1,y1,x,y);
+		delay(50);
+
+	}
+
+    getch();
+    closegraph();
+    return 0;
+}
+
+void circlebres(int x1,int y1,int x, int y){
+        putpixel(x1+x,y1+y,WHITE);
+        putpixel(x1+x,y1-y,WHITE);
+		putpixel(x1-x,y1+y,WHITE);
+		putpixel(x1-x,y1-y,WHITE);
+		putpixel(x1+y,y1+x,WHITE);
+		putpixel(x1+y,y1-x,WHITE);
+		putpixel(x1-y,y1+x,WHITE);
+		putpixel(x1-y,y1-x,WHITE);
+}
